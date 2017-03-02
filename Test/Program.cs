c0c362dd608ad9a16241f3ab5c1f957bc6397656
懒得勤快 <1170397736@qq.com>
2017-03-02 15:00:29 +0800

    规范化

---
+++ Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using Masuit.Tools.Files;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream(@"D:\boot.vmdk", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            {
+                fs.CopyToFileAsync(@"D:\1.bak");
+            }
+            Console.WriteLine("复制完成");
+            Console.ReadKey();
+        }
+    }
+}
