c7c0d64201fe0e2f03a87868e0614c9e756ea326
懒得勤快 <1170397736@qq.com>
2017-03-27 14:02:01 +0800

    新增对象的严格比较扩展方法

---
+++ Masuit.Tools/Objects/ObjectExt.cs
@@ -0,0 +1,19 @@
+﻿namespace Masuit.Tools.Objects
+{
+    /// <summary>
+    /// Object扩展类
+    /// </summary>
+    public static class ObjectExt
+    {
+        /// <summary>
+        /// 严格比较两个对象是否是同一对象
+        /// </summary>
+        /// <param name="_this">自己</param>
+        /// <param name="o">需要比较的对象</param>
+        /// <returns>是否同一对象</returns>
+        public static bool ReferenceEquals(this object _this, object o)
+        {
+            return object.ReferenceEquals(_this, o);
+        }
+    }
+}
