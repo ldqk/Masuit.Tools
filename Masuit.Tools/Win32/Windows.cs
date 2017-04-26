9e88ec960f3757b8e24036187132e1d7e11aa6c5
懒得勤快 <1170397736@qq.com>
2017-04-26 10:59:03 +0800

    新增清理系统内存的方法

---
+++ Masuit.Tools/Win32/Windows.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Masuit.Tools.Hardware;
+
+namespace Masuit.Tools.Win32
+{
+    class Windows
+    {
+        /// <summary>
+        /// 跨平台调用C++的方法
+        /// </summary>
+        /// <param name="hwProc">程序句柄</param>
+        /// <returns></returns>
+        [DllImport("psapi.dll")]
+        static extern int EmptyWorkingSet(IntPtr hwProc);
+
+        /// <summary>
+        /// 清理系统内存，返回优化内存后的内存占用率
+        /// </summary>
+        /// <returns>优化内存后的内存占用率</returns>
+        public static double ClearMemory()
+        {
+            ClearMemorySilent();
+            Thread.Sleep(1000);
+            return SystemInfo.GetRamInfo().MemoryUsage;
+        }
+
+        /// <summary>
+        /// 静默清理系统内存
+        /// </summary>
+        public static void ClearMemorySilent()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            foreach (Process p in Process.GetProcesses())
+            {
+                using (p)
+                {
+                    if ((p.ProcessName.Equals("System")) && (p.ProcessName.Equals("Idle")))
+                    {
+                        //两个系统的关键进程，不整理
+                        continue;
+                    }
+                    try
+                    {
+                        EmptyWorkingSet(p.Handle);
+                    }
+                    catch (Exception e)
+                    {
+                        // ignored
+                    }
+                }
+            }
+        }
+    }
+}
