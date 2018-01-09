dd11a1546c1797be726bac686f9d4336da0f3bd2
懒得勤快 <chenyu0@outlook.com>
2018-01-09 12:57:46 +0800

    .

---
+++ Masuit.Tools.Core/Net/HttpContext2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Masuit.Tools.Core.Net
+{
+    /// <summary>
+    /// 实现类似于.NET Framework中的HttpContext静态对象，使用前需要在Startup中注入，ConfigureServices方法中：services.AddHttpContextAccessor();，Configure方法中：app.UseStaticHttpContext();
+    /// </summary>
+    public static class HttpContext2
+    {
+        private static IHttpContextAccessor _accessor;
+
+        /// <summary>
+        /// 获取当前禽求上下文
+        /// </summary>
+        public static HttpContext Current => _accessor.HttpContext;
+
+        internal static void Configure(IHttpContextAccessor accessor)
+        {
+            _accessor = accessor;
+        }
+    }
+}
