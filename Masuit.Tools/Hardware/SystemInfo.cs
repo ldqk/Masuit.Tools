a2f28fe5433ee590487555a45a5b43c596cb861d
懒得勤快 <chenyu0@outlook.com>
2018-01-24 10:40:12 +0800

    新增获取本机正在使用的外网IP地址

--- Masuit.Tools/Hardware/SystemInfo.cs
+++ Masuit.Tools/Hardware/SystemInfo.cs
@@ -3,8 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Management;
+using System.Net;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
 using Masuit.Tools.Logging;
 using Microsoft.Win32;
 
@@ -491,6 +494,82 @@ namespace Masuit.Tools.Hardware
                 return new List<string>() { "未能获取到操作系统版本，可能是当前程序无管理员权限，如果是web应用程序，请将应用程序池的高级设置中的进程模型下的标识设置为：LocalSystem；如果是普通桌面应用程序，请提升管理员权限后再操作。" };
             }
         }
+
+        /// <summary>  
+        /// 获取当前使用的IP  
+        /// </summary>  
+        /// <returns></returns>  
+        public static string GetLocalUsedIP()
+        {
+            string result = RunApp("route", "print", true);
+            Match m = Regex.Match(result, @"0.0.0.0\s+0.0.0.0\s+(\d+.\d+.\d+.\d+)\s+(\d+.\d+.\d+.\d+)");
+            if (m.Success)
+            {
+                return m.Groups[2].Value;
+            }
+            try
+            {
+                System.Net.Sockets.TcpClient c = new System.Net.Sockets.TcpClient();
+                c.Connect("www.baidu.com", 80);
+                string ip = ((IPEndPoint)c.Client.LocalEndPoint).Address.ToString();
+                c.Close();
+                return ip;
+            }
+            catch (Exception)
+            {
+                return "未能获取到操作系统版本，可能是当前程序无管理员权限，如果是web应用程序，请将应用程序池的高级设置中的进程模型下的标识设置为：LocalSystem；如果是普通桌面应用程序，请提升管理员权限后再操作。";
+            }
+        }
+
+        /// <summary>  
+        /// 运行一个控制台程序并返回其输出参数。  
+        /// </summary>  
+        /// <param name="filename">程序名</param>  
+        /// <param name="arguments">输入参数</param>  
+        /// <returns></returns>  
+        public static string RunApp(string filename, string arguments, bool recordLog)
+        {
+            try
+            {
+                if (recordLog)
+                {
+                    Trace.WriteLine(filename + " " + arguments);
+                }
+
+                Process proc = new Process
+                {
+                    StartInfo = {
+                        FileName = filename,
+                        CreateNoWindow = true,
+                        Arguments = arguments,
+                        RedirectStandardOutput = true,
+                        UseShellExecute = false
+                    }
+                };
+                proc.Start();
+
+                using (System.IO.StreamReader sr = new System.IO.StreamReader(proc.StandardOutput.BaseStream, Encoding.Default))
+                {
+                    Thread.Sleep(100);           //貌似调用系统的nslookup还未返回数据或者数据未编码完成，程序就已经跳过直接执行  
+                                                 //txt = sr.ReadToEnd()了，导致返回的数据为空，故睡眠令硬件反应  
+                    if (!proc.HasExited)         //在无参数调用nslookup后，可以继续输入命令继续操作，如果进程未停止就直接执行  
+                    {                            //txt = sr.ReadToEnd()程序就在等待输入，而且又无法输入，直接掐住无法继续运行  
+                        proc.Kill();
+                    }
+                    string txt = sr.ReadToEnd();
+                    sr.Close();
+                    if (recordLog)
+                        Trace.WriteLine(txt);
+                    return txt;
+                }
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine(ex);
+                return ex.Message;
+            }
+        }
+
         /// <summary>
         /// 获取操作系统版本
         /// </summary>
