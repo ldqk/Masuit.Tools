1175a26e1ec1ef22795fc0b1bb773a5ed8989dd9
懒得勤快 <admin@masuit.com>
2018-03-07 20:49:13 +0800

    Masuit.Tools.Core更改为netcore2.0项目

---
+++ NetCoreTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Masuit.Tools.Core.NoSQL;
+
+namespace NetCoreTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RedisHelper helper = new RedisHelper();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
