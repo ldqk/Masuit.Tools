035b7c5485811de85dda96460defa04ca21578dc
懒得勤快 <admin@masuit.com>
2018-03-11 20:24:42 +0800

    定长队列

---
+++ Masuit.Tools/Systems/LimitedQueue.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Masuit.Tools.Systems
+{
+    /// <summary>
+    /// 定长队列
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class LimitedQueue<T> : Queue<T>
+    {
+        public int Limit { get; set; }
+
+        public LimitedQueue(int limit) : base(limit)
+        {
+            Limit = limit;
+        }
+
+        public new void Enqueue(T item)
+        {
+            if (Count >= Limit)
+            {
+                Dequeue();
+            }
+
+            base.Enqueue(item);
+        }
+    }
+}
