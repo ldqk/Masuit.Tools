aa50211edc6150a66844f6fe38e491dabe549d92
懒得勤快 <admin@masuit.com>
2018-03-25 17:54:42 +0800

    .

--- Masuit.Tools/Properties/AssemblyInfo.cs
+++ Masuit.Tools/Properties/AssemblyInfo.cs
@@ -37,6 +37,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 [assembly: AssemblyVersion("1.9.5.0")]
-[assembly: AssemblyFileVersion("1.9.4.1")]
+[assembly: AssemblyFileVersion("1.9.5.0")]
 [assembly: NeutralResourcesLanguage("zh-CN")]
 
