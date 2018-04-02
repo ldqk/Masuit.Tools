287bebc3781cbc11330249706a4f89d8c56e05bb
懒得勤快 <chenyu0@outlook.com>
2018-04-02 16:33:57 +0800

    .

--- Masuit.Tools/Properties/AssemblyInfo.cs
+++ Masuit.Tools/Properties/AssemblyInfo.cs
@@ -36,7 +36,7 @@ using System.Runtime.InteropServices;
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.9.5.2")]
-[assembly: AssemblyFileVersion("1.9.5.2")]
+[assembly: AssemblyVersion("1.9.5.3")]
+[assembly: AssemblyFileVersion("1.9.5.3")]
 [assembly: NeutralResourcesLanguage("zh-CN")]
 
