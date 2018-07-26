4af896a962bce5ef16e00bf47a3cf7262a4731a8
懒得勤快 <chenyu0@outlook.com>
2018-07-26 14:47:58 +0800

    .

--- Masuit.Tools/Properties/AssemblyInfo.cs
+++ Masuit.Tools/Properties/AssemblyInfo.cs
@@ -37,6 +37,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("1.9.11.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
 [assembly: NeutralResourcesLanguage("zh-CN")]
 
