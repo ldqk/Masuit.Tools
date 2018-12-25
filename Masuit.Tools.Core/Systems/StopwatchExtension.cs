4182ece5fca02d891acde36b8dd860fc97cf1d17
懒得勤快 <admin@masuit.com>
2018-12-25 13:19:08 +0800

    .

--- Masuit.Tools.Core/Systems/StopwatchExtension.cs
+++ Masuit.Tools.Core/Systems/StopwatchExtension.cs
@@ -3,8 +3,16 @@ using System.Diagnostics;
 
 namespace Masuit.Tools.Systems
 {
+    /// <summary>
+    /// stopwatch扩展
+    /// </summary>
     public static class StopwatchExtension
     {
+        /// <summary>
+        /// 检测方法执行时间
+        /// </summary>
+        /// <param name="action"></param>
+        /// <returns></returns>
         public static double Execute(Action action)
         {
             Stopwatch sw = Stopwatch.StartNew();
