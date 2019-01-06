ca32d6d46686ae010d3aa0ba0ec7f5d182780908
懒得勤快 <admin@masuit.com>
2019-01-06 17:36:59 +0800

    断点续传扩展

--- Masuit.Tools.Core/AspNetCore/Extensions/ControllerExtensions.cs
+++ Masuit.Tools.Core/AspNetCore/Extensions/ControllerExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Masuit.Tools.AspNetCore.ResumeFileResults.ResumeFileResult;
+﻿using Masuit.Tools.AspNetCore.Mime;
+using Masuit.Tools.AspNetCore.ResumeFileResults.ResumeFileResult;
 using Microsoft.AspNetCore.Mvc;
 using System.IO;
 
@@ -9,18 +10,7 @@ namespace Masuit.Tools.AspNetCore.ResumeFileResults.Extensions
     /// </summary>
     public static class ControllerExtensions
     {
-        /// <summary>
-        /// 可断点续传和多线程下载的FileResult
-        /// </summary>
-        /// <param name="controller"></param>
-        /// <param name="fileContents">文件二进制流</param>
-        /// <param name="contentType">Content-Type</param>
-        /// <returns></returns>
-        public static ResumeFileContentResult ResumeFile(this ControllerBase controller, byte[] fileContents, string contentType)
-        {
-            return ResumeFile(controller, fileContents, contentType, fileDownloadName: null);
-        }
-
+        private static readonly IMimeMapper _mimeMapper = new MimeMapper();
         /// <summary>
         /// 可断点续传和多线程下载的FileResult
         /// </summary>
@@ -31,7 +21,19 @@ namespace Masuit.Tools.AspNetCore.ResumeFileResults.Extensions
         /// <returns></returns>
         public static ResumeFileContentResult ResumeFile(this ControllerBase controller, byte[] fileContents, string contentType, string fileDownloadName)
         {
-            return ResumeFile(controller, fileContents, contentType, fileDownloadName, etag: null);
+            return ResumeFile(controller, fileContents, contentType, fileDownloadName, null);
+        }
+
+        /// <summary>
+        /// 可断点续传和多线程下载的FileResult
+        /// </summary>
+        /// <param name="controller"></param>
+        /// <param name="fileContents">文件二进制流</param>
+        /// <param name="fileDownloadName">下载的文件名</param>
+        /// <returns></returns>
+        public static ResumeFileContentResult ResumeFile(this ControllerBase controller, byte[] fileContents, string fileDownloadName)
+        {
+            return ResumeFile(controller, fileContents, _mimeMapper.GetMimeFromPath(fileDownloadName), fileDownloadName, null);
         }
 
         /// <summary>
@@ -57,10 +59,11 @@ namespace Masuit.Tools.AspNetCore.ResumeFileResults.Extensions
         /// <param name="controller"></param>
         /// <param name="fileStream">文件二进制流</param>
         /// <param name="contentType">Content-Type</param>
+        /// <param name="fileDownloadName">下载的文件名</param>
         /// <returns></returns>
-        public static ResumeFileStreamResult ResumeFile(this ControllerBase controller, FileStream fileStream, string contentType)
+        public static ResumeFileStreamResult ResumeFile(this ControllerBase controller, FileStream fileStream, string contentType, string fileDownloadName)
         {
-            return ResumeFile(controller, fileStream, contentType, fileDownloadName: null);
+            return ResumeFile(controller, fileStream, contentType, fileDownloadName, null);
         }
 
         /// <summary>
@@ -68,12 +71,11 @@ namespace Masuit.Tools.AspNetCore.ResumeFileResults.Extensions
         /// </summary>
         /// <param name="controller"></param>
         /// <param name="fileStream">文件二进制流</param>
-        /// <param name="contentType">Content-Type</param>
         /// <param name="fileDownloadName">下载的文件名</param>
         /// <returns></returns>
-        public static ResumeFileStreamResult ResumeFile(this ControllerBase controller, FileStream fileStream, string contentType, string fileDownloadName)
+        public static ResumeFileStreamResult ResumeFile(this ControllerBase controller, FileStream fileStream, string fileDownloadName)
         {
-            return ResumeFile(controller, fileStream, contentType, fileDownloadName, etag: null);
+            return ResumeFile(controller, fileStream, _mimeMapper.GetMimeFromPath(fileDownloadName), fileDownloadName, null);
         }
 
         /// <summary>
@@ -99,10 +101,11 @@ namespace Masuit.Tools.AspNetCore.ResumeFileResults.Extensions
         /// <param name="controller"></param>
         /// <param name="virtualPath">服务端本地文件的虚拟路径</param>
         /// <param name="contentType">Content-Type</param>
+        /// <param name="fileDownloadName">下载的文件名</param>
         /// <returns></returns>
-        public static ResumeVirtualFileResult ResumeFile(this ControllerBase controller, string virtualPath, string contentType)
+        public static ResumeVirtualFileResult ResumeFile(this ControllerBase controller, string virtualPath, string contentType, string fileDownloadName)
         {
-            return ResumeFile(controller, virtualPath, contentType, fileDownloadName: null);
+            return ResumeFile(controller, virtualPath, contentType, fileDownloadName, null);
         }
 
         /// <summary>
@@ -110,12 +113,11 @@ namespace Masuit.Tools.AspNetCore.ResumeFileResults.Extensions
         /// </summary>
         /// <param name="controller"></param>
         /// <param name="virtualPath">服务端本地文件的虚拟路径</param>
-        /// <param name="contentType">Content-Type</param>
         /// <param name="fileDownloadName">下载的文件名</param>
         /// <returns></returns>
-        public static ResumeVirtualFileResult ResumeFile(this ControllerBase controller, string virtualPath, string contentType, string fileDownloadName)
+        public static ResumeVirtualFileResult ResumeFile(this ControllerBase controller, string virtualPath, string fileDownloadName)
         {
-            return ResumeFile(controller, virtualPath, contentType, fileDownloadName, etag: null);
+            return ResumeFile(controller, virtualPath, _mimeMapper.GetMimeFromPath(virtualPath), fileDownloadName, null);
         }
 
         /// <summary>
@@ -141,10 +143,11 @@ namespace Masuit.Tools.AspNetCore.ResumeFileResults.Extensions
         /// <param name="controller"></param>
         /// <param name="physicalPath">服务端本地文件的物理路径</param>
         /// <param name="contentType">Content-Type</param>
+        /// <param name="fileDownloadName">下载的文件名</param>
         /// <returns></returns>
-        public static ResumePhysicalFileResult ResumePhysicalFile(this ControllerBase controller, string physicalPath, string contentType)
+        public static ResumePhysicalFileResult ResumePhysicalFile(this ControllerBase controller, string physicalPath, string contentType, string fileDownloadName)
         {
-            return ResumePhysicalFile(controller, physicalPath, contentType, fileDownloadName: null, etag: null);
+            return ResumePhysicalFile(controller, physicalPath, contentType, fileDownloadName, etag: null);
         }
 
         /// <summary>
@@ -152,12 +155,11 @@ namespace Masuit.Tools.AspNetCore.ResumeFileResults.Extensions
         /// </summary>
         /// <param name="controller"></param>
         /// <param name="physicalPath">服务端本地文件的物理路径</param>
-        /// <param name="contentType">Content-Type</param>
         /// <param name="fileDownloadName">下载的文件名</param>
         /// <returns></returns>
-        public static ResumePhysicalFileResult ResumePhysicalFile(this ControllerBase controller, string physicalPath, string contentType, string fileDownloadName)
+        public static ResumePhysicalFileResult ResumePhysicalFile(this ControllerBase controller, string physicalPath, string fileDownloadName)
         {
-            return ResumePhysicalFile(controller, physicalPath, contentType, fileDownloadName, etag: null);
+            return ResumePhysicalFile(controller, physicalPath, _mimeMapper.GetMimeFromPath(physicalPath), fileDownloadName, etag: null);
         }
 
         /// <summary>
