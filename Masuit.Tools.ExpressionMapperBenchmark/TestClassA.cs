511a8eafd6df7d0bc18e04e95504193718694eaa
懒得勤快 <admin@masuit.com>
2019-01-19 16:21:48 +0800

    ExpressionMapper跑分测试

--- Masuit.Tools.ExpressionMapperBenchmark/TestClassA.cs
+++ Masuit.Tools.ExpressionMapperBenchmark/TestClassA.cs
@@ -1,34 +1,48 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
-namespace Masuit.Tools.UnitTest.TestClasses
+namespace Masuit.Tools.ExpressionMapperBenchmark
 {
     public class TestClassA
     {
         public string MyProperty { get; set; }
+        public int Int { get; set; }
+        public double Double { get; set; }
+        public DateTime DateTime { get; set; }
 
         public TestClassC TestClassC { get; set; }
         public List<TestClassC> List { get; set; }
-        public TestClassC[] Array { get; set; }
     }
 
     public class TestClassB
     {
         public string MyProperty { get; set; }
+        public int Int { get; set; }
+        public double Double { get; set; }
+        public DateTime DateTime { get; set; }
+
 
         public TestClassC TestClassC { get; set; }
         public List<TestClassD> List { get; set; }
-        public TestClassD[] Array { get; set; }
     }
 
     public class TestClassC
     {
         public string MyProperty { get; set; }
+        public int Int { get; set; }
+        public double Double { get; set; }
+        public DateTime DateTime { get; set; }
+
         public TestClassD Obj { get; set; }
     }
 
     public class TestClassD
     {
         public string MyProperty { get; set; }
+        public int Int { get; set; }
+        public double Double { get; set; }
+        public DateTime DateTime { get; set; }
+
         public TestClassC Obj { get; set; }
     }
 }
