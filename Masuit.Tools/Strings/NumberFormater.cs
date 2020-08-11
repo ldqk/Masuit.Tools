5977a089f04fe608602e5ce79ecb0adde0fa794c
懒得勤快 <admin@masuit.com>
2020-08-11 11:09:57 +0800

    进制转换支持超大数字转换

--- Masuit.Tools/Strings/NumberFormater.cs
+++ Masuit.Tools/Strings/NumberFormater.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Numerics;
 using System.Text.RegularExpressions;
 
 namespace Masuit.Tools.Strings
@@ -49,11 +50,12 @@ namespace Masuit.Tools.Strings
                 bin = 2;
             }
 
-            if (bin > 62)
+            if (bin > 64)
             {
-                throw new ArgumentException("默认进制最大支持62进制");
+                throw new ArgumentException("默认进制最大支持64进制");
             }
-            Characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".Substring(0, bin);
+
+            Characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+/".Substring(0, bin);
         }
 
         /// <summary>
@@ -84,6 +86,33 @@ namespace Masuit.Tools.Strings
         }
 
         /// <summary>
+        /// 数字转换为指定的进制形式字符串
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public string ToString(BigInteger number)
+        {
+            List<string> result = new List<string>();
+            if (number < 0)
+            {
+                number = -number;
+                result.Add("0");
+            }
+
+            BigInteger t = number;
+
+            while (t != 0)
+            {
+                var mod = t % Length;
+                t = BigInteger.Abs(BigInteger.Divide(t, Length));
+                var character = Characters[(int)mod].ToString();
+                result.Insert(0, character);
+            }
+
+            return string.Join("", result);
+        }
+
+        /// <summary>
         /// 指定字符串转换为指定进制的数字形式
         /// </summary>
         /// <param name="str"></param>
@@ -94,12 +123,25 @@ namespace Masuit.Tools.Strings
             return new string(str.ToCharArray().Reverse().ToArray()).Where(ch => Characters.Contains(ch)).Sum(ch => Characters.IndexOf(ch) * (long)Math.Pow(Length, j++));
         }
 
+        /// <summary>
+        /// 指定字符串转换为指定进制的大数形式
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public BigInteger FromStringBig(string str)
+        {
+            int j = 0;
+            var chars = new string(str.ToCharArray().Reverse().ToArray()).Where(ch => Characters.Contains(ch));
+            return chars.Aggregate(BigInteger.Zero, (current, c) => current + Characters.IndexOf(c) * BigInteger.Pow(Length, j++));
+        }
+
         /// <summary>Returns a string that represents the current object.</summary>
         /// <returns>A string that represents the current object.</returns>
         public override string ToString()
         {
             return Length + "模式";
         }
+
         // 转换数字 
         private static char ToNum(char x)
         {
