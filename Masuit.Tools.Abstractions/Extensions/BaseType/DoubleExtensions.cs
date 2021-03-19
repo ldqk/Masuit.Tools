5ccdb392dd062e6184fefb201d0b3e4425cd8648
懒得勤快 <admin@masuit.com>
2021-03-19 14:04:59 +0800

    增加转decimal的专属方法

--- Masuit.Tools.Abstractions/Extensions/BaseType/DoubleExtensions.cs
+++ Masuit.Tools.Abstractions/Extensions/BaseType/DoubleExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace Masuit.Tools
+﻿using System;
+
+namespace Masuit.Tools
 {
     public static class DoubleExtensions
     {
@@ -10,6 +12,48 @@
         public static double Digits8(this double num)
         {
             return (long)(num * 1E+8) * 1e-8;
+        }
+
+        /// <summary>
+        /// 转decimal
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static decimal ToDecimal(this double num)
+        {
+            return num.ConvertTo<decimal>();
+        }
+
+        /// <summary>
+        /// 转decimal
+        /// </summary>
+        /// <param name="num"></param>
+        /// <param name="round">小数位数</param>
+        /// <returns></returns>
+        public static decimal ToDecimal(this double num, int round)
+        {
+            return Math.Round(num.ConvertTo<decimal>(), round);
+        }
+
+        /// <summary>
+        /// 转decimal
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static decimal ToDecimal(this float num)
+        {
+            return num.ConvertTo<decimal>();
+        }
+
+        /// <summary>
+        /// 转decimal
+        /// </summary>
+        /// <param name="num"></param>
+        /// <param name="round">小数位数</param>
+        /// <returns></returns>
+        public static decimal ToDecimal(this float num, int round)
+        {
+            return Math.Round(num.ConvertTo<decimal>(), round);
         }
     }
 }
