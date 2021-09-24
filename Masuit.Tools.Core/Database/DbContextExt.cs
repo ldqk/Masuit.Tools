38afed3f9eb9923156cb4643fcd36145e0663756
懒得勤快 <admin@masuit.com>
2021-09-24 10:47:08 +0800

    新增EFCore变更记录获取

--- Masuit.Tools.Core/Database/DbContextExt.cs
+++ Masuit.Tools.Core/Database/DbContextExt.cs
@@ -7,7 +7,194 @@ using System.Reflection;
 
 namespace Masuit.Tools.Core
 {
-    public class ChangePropertyValue
+    public static class DbContextExt
+    {
+        /// <summary>
+        /// 获取变化的实体信息
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="db"></param>
+        /// <returns></returns>
+        public static IEnumerable<ChangeEntry> GetChanges<T>(this DbContext db)
+        {
+            return db.ChangeTracker.Entries().Where(e => e.State == EntityState.Modified && e.Entity is T).Select(e =>
+            {
+                var originalObject = e.OriginalValues.ToObject();
+                var currentObject = e.CurrentValues.ToObject();
+                return new ChangeEntry
+                {
+                    EntityState = e.State,
+                    Entity = e.Entity,
+                    EntityType = e.OriginalValues.EntityType.ClrType,
+                    ChangeProperties = e.OriginalValues.Properties.Select(p => (Property: p, Value: p.PropertyInfo.GetValue(originalObject))).Zip(e.CurrentValues.Properties.Select(p => (Property: p, Value: p.PropertyInfo.GetValue(currentObject))), (t1, t2) => new ChangePropertyInfo()
+                    {
+                        PropertyInfo = t1.Property.PropertyInfo,
+                        OriginalValue = t1.Value,
+                        CurrentValue = t2.Value,
+                        IsPrimaryKey = t1.Property.IsPrimaryKey(),
+                        IsForeignKey = t1.Property.IsForeignKey()
+                    }).Where(t => Comparer.Default.Compare(t.OriginalValue, t.CurrentValue) != 0).ToList()
+                };
+            });
+        }
+
+        /// <summary>
+        /// 获取变化的实体信息
+        /// </summary>
+        /// <param name="db"></param>
+        /// <returns></returns>
+        public static IEnumerable<ChangeEntry> GetChanges(this DbContext db)
+        {
+            return db.ChangeTracker.Entries().Where(e => e.State == EntityState.Modified).Select(e =>
+            {
+                var originalObject = e.OriginalValues.ToObject();
+                var currentObject = e.CurrentValues.ToObject();
+                return new ChangeEntry()
+                {
+                    EntityState = e.State,
+                    Entity = e.Entity,
+                    EntityType = e.OriginalValues.EntityType.ClrType,
+                    ChangeProperties = e.OriginalValues.Properties.Select(p => (Property: p, Value: p.PropertyInfo.GetValue(originalObject))).Zip(e.CurrentValues.Properties.Select(p => (Property: p, Value: p.PropertyInfo.GetValue(currentObject))), (t1, t2) => new ChangePropertyInfo()
+                    {
+                        PropertyInfo = t1.Property.PropertyInfo,
+                        OriginalValue = t1.Value,
+                        CurrentValue = t2.Value,
+                        IsPrimaryKey = t1.Property.IsPrimaryKey(),
+                        IsForeignKey = t1.Property.IsForeignKey(),
+                    }).Where(t => Comparer.Default.Compare(t.OriginalValue, t.CurrentValue) != 0).ToList()
+                };
+            });
+        }
+
+        /// <summary>
+        /// 获取添加的实体信息
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="db"></param>
+        /// <returns></returns>
+        public static IEnumerable<ChangeEntry> GetAdded<T>(this DbContext db)
+        {
+            return db.ChangeTracker.Entries().Where(e => e.State == EntityState.Added && e.Entity is T).Select(e =>
+            {
+                var currentObject = e.CurrentValues.ToObject();
+                return new ChangeEntry
+                {
+                    EntityState = e.State,
+                    Entity = e.Entity,
+                    EntityType = e.CurrentValues.EntityType.ClrType,
+                    ChangeProperties = e.CurrentValues.Properties.Select(p => new ChangePropertyInfo()
+                    {
+                        PropertyInfo = p.PropertyInfo,
+                        CurrentValue = p.PropertyInfo.GetValue(currentObject),
+                        IsPrimaryKey = p.IsPrimaryKey(),
+                        IsForeignKey = p.IsForeignKey(),
+                    }).ToList()
+                };
+            });
+        }
+
+        /// <summary>
+        /// 获取添加的实体信息
+        /// </summary>
+        /// <param name="db"></param>
+        /// <returns></returns>
+        public static IEnumerable<ChangeEntry> GetAdded(this DbContext db)
+        {
+            return db.ChangeTracker.Entries().Where(e => e.State == EntityState.Added).Select(e =>
+            {
+                var currentObject = e.CurrentValues.ToObject();
+                return new ChangeEntry
+                {
+                    EntityState = e.State,
+                    Entity = e.Entity,
+                    EntityType = e.CurrentValues.EntityType.ClrType,
+                    ChangeProperties = e.CurrentValues.Properties.Select(p => new ChangePropertyInfo()
+                    {
+                        PropertyInfo = p.PropertyInfo,
+                        CurrentValue = p.PropertyInfo.GetValue(currentObject),
+                        IsPrimaryKey = p.IsPrimaryKey(),
+                        IsForeignKey = p.IsForeignKey(),
+                    }).ToList()
+                };
+            });
+        }
+
+        /// <summary>
+        /// 获取移除的实体信息
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="db"></param>
+        /// <returns></returns>
+        public static IEnumerable<ChangeEntry> GetRemoved<T>(this DbContext db)
+        {
+            return db.ChangeTracker.Entries().Where(e => e.State == EntityState.Deleted && e.Entity is T).Select(e =>
+            {
+                var originalObject = e.OriginalValues.ToObject();
+                return new ChangeEntry
+                {
+                    EntityState = e.State,
+                    Entity = e.Entity,
+                    EntityType = e.OriginalValues.EntityType.ClrType,
+                    ChangeProperties = e.OriginalValues.Properties.Select(p => new ChangePropertyInfo()
+                    {
+                        PropertyInfo = p.PropertyInfo,
+                        OriginalValue = p.PropertyInfo.GetValue(originalObject),
+                        IsPrimaryKey = p.IsPrimaryKey(),
+                        IsForeignKey = p.IsForeignKey(),
+                    }).ToList()
+                };
+            });
+        }
+
+        /// <summary>
+        /// 获取移除的实体信息
+        /// </summary>
+        /// <param name="db"></param>
+        /// <returns></returns>
+        public static IEnumerable<ChangeEntry> GetRemoved(this DbContext db)
+        {
+            return db.ChangeTracker.Entries().Where(e => e.State == EntityState.Deleted).Select(e =>
+            {
+                var originalObject = e.OriginalValues.ToObject();
+                return new ChangeEntry
+                {
+                    EntityState = e.State,
+                    Entity = e.Entity,
+                    EntityType = e.OriginalValues.EntityType.ClrType,
+                    ChangeProperties = e.OriginalValues.Properties.Select(p => new ChangePropertyInfo()
+                    {
+                        PropertyInfo = p.PropertyInfo,
+                        OriginalValue = p.PropertyInfo.GetValue(originalObject),
+                        IsPrimaryKey = p.IsPrimaryKey(),
+                        IsForeignKey = p.IsForeignKey(),
+                    }).ToList()
+                };
+            });
+        }
+
+        /// <summary>
+        /// 获取所有的变更信息
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="db"></param>
+        /// <returns></returns>
+        public static IEnumerable<ChangeEntry> GetAllChanges<T>(this DbContext db)
+        {
+            return GetChanges<T>(db).Union(GetAdded<T>(db)).Union(GetRemoved<T>(db));
+        }
+
+        /// <summary>
+        /// 获取所有的变更信息
+        /// </summary>
+        /// <param name="db"></param>
+        /// <returns></returns>
+        public static IEnumerable<ChangeEntry> GetAllChanges(this DbContext db)
+        {
+            return GetChanges(db).Union(GetAdded(db)).Union(GetRemoved(db));
+        }
+    }
+
+    public class ChangePropertyInfo
     {
         /// <summary>
         /// 属性
@@ -25,6 +212,19 @@ namespace Masuit.Tools.Core
         public object CurrentValue { get; set; }
 
         /// <summary>
+        /// 是否是主键
+        /// </summary>
+        public bool IsPrimaryKey { get; set; }
+
+        /// <summary>
+        /// 是否是外键
+        /// </summary>
+        public bool IsForeignKey { get; set; }
+    }
+
+    public class ChangeEntry
+    {
+        /// <summary>
         /// 所属实体
         /// </summary>
         public object Entity { get; set; }
@@ -40,183 +240,8 @@ namespace Masuit.Tools.Core
         public EntityState EntityState { get; set; }
 
         /// <summary>
-        /// 是否是主键
+        /// 字段变更信息
         /// </summary>
-        public bool IsPrimaryKey { get; set; }
-
-        /// <summary>
-        /// 是否是外键
-        /// </summary>
-        public bool IsForeignKey { get; set; }
-
-    }
-
-    public static class DbContextExt
-    {
-        /// <summary>
-        /// 获取变化的实体信息
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="db"></param>
-        /// <returns></returns>
-        public static IEnumerable<ChangePropertyValue> GetChanges<T>(this DbContext db)
-        {
-            return db.ChangeTracker.Entries().Where(e => e.State == EntityState.Modified && e.Entity is T).SelectMany(e =>
-            {
-                var originalObject = e.OriginalValues.ToObject();
-                var currentObject = e.CurrentValues.ToObject();
-                return e.OriginalValues.Properties.Select(p => (Property: p, Value: p.PropertyInfo.GetValue(originalObject))).Zip(e.CurrentValues.Properties.Select(p => (Property: p, Value: p.PropertyInfo.GetValue(currentObject))), (t1, t2) => new ChangePropertyValue()
-                {
-                    PropertyInfo = t1.Property.PropertyInfo,
-                    OriginalValue = t1.Value,
-                    CurrentValue = t2.Value,
-                    EntityState = e.State,
-                    Entity = e.Entity,
-                    IsPrimaryKey = t1.Property.IsPrimaryKey(),
-                    IsForeignKey = t1.Property.IsForeignKey(),
-                    EntityType = e.OriginalValues.EntityType.ClrType
-                }).Where(t => Comparer.Default.Compare(t.OriginalValue, t.CurrentValue) != 0);
-            });
-        }
-
-        /// <summary>
-        /// 获取变化的实体信息
-        /// </summary>
-        /// <param name="db"></param>
-        /// <returns></returns>
-        public static IEnumerable<ChangePropertyValue> GetChanges(this DbContext db)
-        {
-            return db.ChangeTracker.Entries().Where(e => e.State == EntityState.Modified).SelectMany(e =>
-            {
-                var originalObject = e.OriginalValues.ToObject();
-                var currentObject = e.CurrentValues.ToObject();
-                return e.OriginalValues.Properties.Select(p => (Property: p, Value: p.PropertyInfo.GetValue(originalObject))).Zip(e.CurrentValues.Properties.Select(p => (Property: p, Value: p.PropertyInfo.GetValue(currentObject))), (t1, t2) => new ChangePropertyValue()
-                {
-                    PropertyInfo = t1.Property.PropertyInfo,
-                    OriginalValue = t1.Value,
-                    CurrentValue = t2.Value,
-                    EntityState = e.State,
-                    Entity = e.Entity,
-                    IsPrimaryKey = t1.Property.IsPrimaryKey(),
-                    IsForeignKey = t1.Property.IsForeignKey(),
-                    EntityType = e.OriginalValues.EntityType.ClrType
-                }).Where(t => Comparer.Default.Compare(t.OriginalValue, t.CurrentValue) != 0);
-            });
-        }
-
-        /// <summary>
-        /// 获取添加的实体信息
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="db"></param>
-        /// <returns></returns>
-        public static IEnumerable<ChangePropertyValue> GetAdded<T>(this DbContext db)
-        {
-            return db.ChangeTracker.Entries().Where(e => e.State == EntityState.Added && e.Entity is T).SelectMany(e =>
-            {
-                var currentObject = e.CurrentValues.ToObject();
-                return e.CurrentValues.Properties.Select(p => (Property: p, Value: p.PropertyInfo.GetValue(currentObject))).Select(t => new ChangePropertyValue
-                {
-                    PropertyInfo = t.Property.PropertyInfo,
-                    CurrentValue = t.Value,
-                    EntityState = e.State,
-                    Entity = e.Entity,
-                    IsPrimaryKey = t.Property.IsPrimaryKey(),
-                    IsForeignKey = t.Property.IsForeignKey(),
-                    EntityType = e.OriginalValues.EntityType.ClrType
-                });
-            });
-        }
-
-        /// <summary>
-        /// 获取添加的实体信息
-        /// </summary>
-        /// <param name="db"></param>
-        /// <returns></returns>
-        public static IEnumerable<ChangePropertyValue> GetAdded(this DbContext db)
-        {
-            return db.ChangeTracker.Entries().Where(e => e.State == EntityState.Added).SelectMany(e =>
-            {
-                var currentObject = e.CurrentValues.ToObject();
-                return e.CurrentValues.Properties.Select(p => (Property: p, Value: p.PropertyInfo.GetValue(currentObject))).Select(t => new ChangePropertyValue
-                {
-                    PropertyInfo = t.Property.PropertyInfo,
-                    CurrentValue = t.Value,
-                    EntityState = e.State,
-                    Entity = e.Entity,
-                    IsPrimaryKey = t.Property.IsPrimaryKey(),
-                    IsForeignKey = t.Property.IsForeignKey(),
-                    EntityType = e.OriginalValues.EntityType.ClrType
-                });
-            });
-        }
-
-        /// <summary>
-        /// 获取移除的实体信息
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="db"></param>
-        /// <returns></returns>
-        public static IEnumerable<ChangePropertyValue> GetRemoved<T>(this DbContext db)
-        {
-            return db.ChangeTracker.Entries().Where(e => e.State == EntityState.Deleted && e.Entity is T).SelectMany(e =>
-            {
-                var originalObject = e.OriginalValues.ToObject();
-                return e.OriginalValues.Properties.Select(p => (Property: p, Value: p.PropertyInfo.GetValue(originalObject))).Select(t => new ChangePropertyValue
-                {
-                    PropertyInfo = t.Property.PropertyInfo,
-                    OriginalValue = t.Value,
-                    EntityState = e.State,
-                    Entity = e.Entity,
-                    IsPrimaryKey = t.Property.IsPrimaryKey(),
-                    IsForeignKey = t.Property.IsForeignKey(),
-                    EntityType = e.OriginalValues.EntityType.ClrType
-                });
-            });
-        }
-
-        /// <summary>
-        /// 获取移除的实体信息
-        /// </summary>
-        /// <param name="db"></param>
-        /// <returns></returns>
-        public static IEnumerable<ChangePropertyValue> GetRemoved(this DbContext db)
-        {
-            return db.ChangeTracker.Entries().Where(e => e.State == EntityState.Deleted).SelectMany(e =>
-            {
-                var originalObject = e.OriginalValues.ToObject();
-                return e.OriginalValues.Properties.Select(p => (Property: p, Value: p.PropertyInfo.GetValue(originalObject))).Select(t => new ChangePropertyValue
-                {
-                    PropertyInfo = t.Property.PropertyInfo,
-                    OriginalValue = t.Value,
-                    EntityState = e.State,
-                    Entity = e.Entity,
-                    IsPrimaryKey = t.Property.IsPrimaryKey(),
-                    IsForeignKey = t.Property.IsForeignKey(),
-                    EntityType = e.OriginalValues.EntityType.ClrType
-                });
-            });
-        }
-
-        /// <summary>
-        /// 获取所有的变更信息
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="db"></param>
-        /// <returns></returns>
-        public static IEnumerable<ChangePropertyValue> GetAllChanges<T>(this DbContext db)
-        {
-            return GetChanges<T>(db).Union(GetAdded<T>(db)).Union(GetRemoved<T>(db));
-        }
-
-        /// <summary>
-        /// 获取所有的变更信息
-        /// </summary>
-        /// <param name="db"></param>
-        /// <returns></returns>
-        public static IEnumerable<ChangePropertyValue> GetAllChanges(this DbContext db)
-        {
-            return GetChanges(db).Union(GetAdded(db)).Union(GetRemoved(db));
-        }
+        public List<ChangePropertyInfo> ChangeProperties { get; set; }
     }
 }
