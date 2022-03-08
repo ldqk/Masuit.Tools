53bc4db84f584703601c03c462540ea293f8d7d3
懒得勤快 <admin@masuit.com>
2022-03-08 20:27:56 +0800

    ++

---
+++ Masuit.Tools.AspNetCore/ModelBinder/BodyOrDefaultBindingSource.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Masuit.Tools.AspNetCore.ModelBinder;
+
+public class BodyOrDefaultBindingSource : BindingSource
+{
+    public static readonly BindingSource BodyOrDefault = new BodyOrDefaultBindingSource("BodyOrDefault", "BodyOrDefault", true, true);
+
+    public BodyOrDefaultBindingSource(string id, string displayName, bool isGreedy, bool isFromRequest) : base(id, displayName, isGreedy, isFromRequest)
+    {
+    }
+
+    public override bool CanAcceptDataFrom(BindingSource bindingSource)
+    {
+        return bindingSource == Body || bindingSource == this;
+    }
+}
---
+++ Masuit.Tools.AspNetCore/ModelBinder/FromBodyOrDefaultAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Masuit.Tools.AspNetCore.ModelBinder;
+
+[AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Property)]
+public class FromBodyOrDefaultAttribute : Attribute, IBindingSourceMetadata
+{
+    public BindingSource BindingSource => BodyOrDefaultBindingSource.BodyOrDefault;
+}
