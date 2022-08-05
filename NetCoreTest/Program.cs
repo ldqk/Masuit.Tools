2957c0ffc402cb8a39a9181723dfabd3e7d71cfd
懒得勤快 <admin@masuit.com>
2022-08-05 13:30:00 +0800

    *

--- NetCoreTest/Program.cs
+++ NetCoreTest/Program.cs
@@ -1,6 +1,26 @@
 ﻿using System;
-using System.IO;
-using Masuit.Tools.Files.FileDetector;
+using Masuit.Tools.Systems;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using NetCoreTest;
+using Newtonsoft.Json;
 
-var detector = File.OpenRead(@"E:\下载\Part1.mkv").DetectFiletype();
-Console.WriteLine(detector.Extension);
+string json1 = "{\"a\":\"aa\"}";
+string json2 = "{\"b\":\"bb\"}";
+string json3 = "{\"MyProperty\":\"mm\"}";
+JsonConvert.DefaultSettings = () => new JsonSerializerSettings() { ContractResolver = new CompositeContractResolver() };
+var m1 = JsonConvert.DeserializeObject<MyClass>(json1);
+var m2 = JsonConvert.DeserializeObject<MyClass>(json2);
+var m3 = JsonConvert.DeserializeObject<MyClass>(json3);
+Console.ReadKey();
+
+//CreateWebHostBuilder(args).Build().Run();
+static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+    WebHost.CreateDefaultBuilder(args)
+        .UseStartup<Startup>();
+
+public class MyClass
+{
+    [SerializeIgnore, FallbackJsonProperty(nameof(MyProperty), "a", "b")]
+    public string MyProperty { get; set; }
+}
