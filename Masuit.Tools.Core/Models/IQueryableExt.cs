4e1f4f8da31409ae4be2e45ca181c24f94e69b27
懒得勤快 <admin@masuit.com>
2022-09-22 21:55:52 +0800

    修正算数计算异常

--- Masuit.Tools.Core/Models/IQueryableExt.cs
+++ Masuit.Tools.Core/Models/IQueryableExt.cs
@@ -1,85 +1,80 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace Masuit.Tools.Models
+namespace Masuit.Tools.Models;
+
+public static partial class IQueryableExt
 {
-    public static partial class IQueryableExt
+    /// <summary>
+    /// 生成分页集合
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="query"></param>
+    /// <param name="page">当前页</param>
+    /// <param name="size">页大小</param>
+    /// <returns></returns>
+    public static async Task<PagedList<T>> ToPagedListAsync<T>(this IQueryable<T> query, int page, int size)
     {
-        /// <summary>
-        /// 生成分页集合
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="query"></param>
-        /// <param name="page">当前页</param>
-        /// <param name="size">页大小</param>
-        /// <returns></returns>
-        public static async Task<PagedList<T>> ToPagedListAsync<T>(this IQueryable<T> query, int page, int size)
+        var totalCount = await query.CountAsync();
+        if (1L * page * size > totalCount)
         {
-            var totalCount = await query.CountAsync();
-            if (page * size > totalCount)
-            {
-                page = (int)Math.Ceiling(totalCount / (size * 1.0));
-            }
-
-            if (page <= 0)
-            {
-                page = 1;
-            }
-
-            var list = await query.Skip(size * (page - 1)).Take(size).ToListAsync();
-            return new PagedList<T>(list, page, size, totalCount);
-        }
-        /// <summary>
-        /// 生成分页集合
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="query"></param>
-        /// <param name="page">当前页</param>
-        /// <param name="size">页大小</param>
-        /// <returns></returns>
-        public static PagedList<T> ToPagedList<T>(this IQueryable<T> query, int page, int size)
-        {
-            var totalCount = query.Count();
-            if (page * size > totalCount)
-            {
-                page = (int)Math.Ceiling(totalCount / (size * 1.0));
-            }
-
-            if (page <= 0)
-            {
-                page = 1;
-            }
-
-            var list = query.Skip(size * (page - 1)).Take(size).ToList();
-            return new PagedList<T>(list, page, size, totalCount);
+            page = (int)Math.Ceiling(totalCount / (size * 1.0));
         }
 
-        /// <summary>
-        /// 生成分页集合
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="query"></param>
-        /// <param name="page">当前页</param>
-        /// <param name="size">页大小</param>
-        /// <returns></returns>
-        public static PagedList<T> ToPagedList<T>(this IEnumerable<T> query, int page, int size)
+        if (page <= 0)
         {
-            var totalCount = query.Count();
-            if (page * size > totalCount)
-            {
-                page = (int)Math.Ceiling(totalCount / (size * 1.0));
-            }
-
-            if (page <= 0)
-            {
-                page = 1;
-            }
-
-            var list = query.Skip(size * (page - 1)).Take(size).ToList();
-            return new PagedList<T>(list, page, size, totalCount);
+            page = 1;
         }
+
+        var list = await query.Skip(size * (page - 1)).Take(size).ToListAsync();
+        return new PagedList<T>(list, page, size, totalCount);
+    }
+    /// <summary>
+    /// 生成分页集合
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="query"></param>
+    /// <param name="page">当前页</param>
+    /// <param name="size">页大小</param>
+    /// <returns></returns>
+    public static PagedList<T> ToPagedList<T>(this IQueryable<T> query, int page, int size)
+    {
+        var totalCount = query.Count();
+        if (1L * page * size > totalCount)
+        {
+            page = (int)Math.Ceiling(totalCount / (size * 1.0));
+        }
+
+        if (page <= 0)
+        {
+            page = 1;
+        }
+
+        var list = query.Skip(size * (page - 1)).Take(size).ToList();
+        return new PagedList<T>(list, page, size, totalCount);
+    }
+
+    /// <summary>
+    /// 生成分页集合
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="query"></param>
+    /// <param name="page">当前页</param>
+    /// <param name="size">页大小</param>
+    /// <returns></returns>
+    public static PagedList<T> ToPagedList<T>(this IEnumerable<T> query, int page, int size)
+    {
+        var totalCount = query.Count();
+        if (1L * page * size > totalCount)
+        {
+            page = (int)Math.Ceiling(totalCount / (size * 1.0));
+        }
+
+        if (page <= 0)
+        {
+            page = 1;
+        }
+
+        var list = query.Skip(size * (page - 1)).Take(size).ToList();
+        return new PagedList<T>(list, page, size, totalCount);
     }
 }
