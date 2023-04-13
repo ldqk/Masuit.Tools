69f406fa0e7fb25411ed336b9ce3a4c7e7fc227c
懒得勤快 <admin@masuit.com>
2023-04-13 11:54:45 +0800

    +

--- Masuit.Tools.Core/Database/DbContextExt.cs
+++ Masuit.Tools.Core/Database/DbContextExt.cs
@@ -267,6 +267,50 @@ namespace Masuit.Tools.Core
             return result;
         }
 
+        public static async Task<T> FirstOrDefaultWithNoLockAsync<T>(this IQueryable<T> query, Expression<Func<T, bool>> where, CancellationToken cancellationToken = default)
+        {
+            using var scope = new TransactionScope(TransactionScopeOption.Required, new TransactionOptions()
+            {
+                IsolationLevel = IsolationLevel.ReadUncommitted
+            }, TransactionScopeAsyncFlowOption.Enabled);
+            var result = await query.FirstOrDefaultAsync(where, cancellationToken);
+            scope.Complete();
+            return result;
+        }
+
+        public static async Task<T> FirstOrDefaultWithNoLockAsync<T>(this IQueryable<T> query, CancellationToken cancellationToken = default)
+        {
+            using var scope = new TransactionScope(TransactionScopeOption.Required, new TransactionOptions()
+            {
+                IsolationLevel = IsolationLevel.ReadUncommitted
+            }, TransactionScopeAsyncFlowOption.Enabled);
+            var result = await query.FirstOrDefaultAsync(cancellationToken);
+            scope.Complete();
+            return result;
+        }
+
+        public static async Task<T> SingleOrDefaultWithNoLockAsync<T>(this IQueryable<T> query, Expression<Func<T, bool>> where, CancellationToken cancellationToken = default)
+        {
+            using var scope = new TransactionScope(TransactionScopeOption.Required, new TransactionOptions()
+            {
+                IsolationLevel = IsolationLevel.ReadUncommitted
+            }, TransactionScopeAsyncFlowOption.Enabled);
+            var result = await query.SingleOrDefaultAsync(where, cancellationToken);
+            scope.Complete();
+            return result;
+        }
+
+        public static async Task<T> SingleOrDefaultWithNoLockAsync<T>(this IQueryable<T> query, CancellationToken cancellationToken = default)
+        {
+            using var scope = new TransactionScope(TransactionScopeOption.Required, new TransactionOptions()
+            {
+                IsolationLevel = IsolationLevel.ReadUncommitted
+            }, TransactionScopeAsyncFlowOption.Enabled);
+            var result = await query.SingleOrDefaultAsync(cancellationToken);
+            scope.Complete();
+            return result;
+        }
+
         public static async Task<bool> AllWithNoLockAsync<T>(this IQueryable<T> query, Expression<Func<T, bool>> where, CancellationToken cancellationToken = default)
         {
             using var scope = new TransactionScope(TransactionScopeOption.Required, new TransactionOptions()
