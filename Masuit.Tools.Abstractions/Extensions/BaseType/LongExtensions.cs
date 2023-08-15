74d4c9623075076ce758d1f63ff3b7f64433aeef
lazy y chen <lazy.y.chen@cn.pwc.com>
2023-08-15 15:35:49 +0800

    +

--- Masuit.Tools.Abstractions/Extensions/BaseType/LongExtensions.cs
+++ Masuit.Tools.Abstractions/Extensions/BaseType/LongExtensions.cs
@@ -26,5 +26,27 @@ namespace Masuit.Tools
         {
             return BitConverter.GetBytes(value);
         }
+
+        /// <summary>
+        /// 十进制转任意进制
+        /// </summary>
+        /// <param name="num"></param>
+        /// <param name="newBase">进制</param>
+        /// <returns></returns>
+        public static string ToBase(this ulong num, byte newBase)
+        {
+            var nf = new NumberFormater(newBase);
+            return nf.ToString(num);
+        }
+
+        /// <summary>
+        /// 转换成字节数组
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static byte[] GetBytes(this ulong value)
+        {
+            return BitConverter.GetBytes(value);
+        }
     }
 }
