f2842951557effd8229f92c06fd1ae95e7fbeefd
懒得勤快 <admin@masuit.com>
2023-09-02 23:52:33 +0800

    获取主板信息的GUID和UUID

--- Masuit.Tools.Abstractions/Hardware/BiosInfo.cs
+++ Masuit.Tools.Abstractions/Hardware/BiosInfo.cs
@@ -5,31 +5,34 @@
 /// </summary>
 public class BiosInfo
 {
-    private static readonly BiosInfo _local = SystemInfo.GetBiosInfo();
+	private static readonly BiosInfo _local = SystemInfo.GetBiosInfo();
 
-    /// <summary>
-    /// 本地实例
-    /// </summary>
-    public static BiosInfo Local => _local;
+	/// <summary>
+	/// 本地实例
+	/// </summary>
+	public static BiosInfo Local => _local;
 
-    /// <summary>
-    /// ID
-    /// </summary>
-    public string ID { get; set; }
+	/// <summary>
+	/// ID
+	/// </summary>
+	public string ID { get; set; }
 
-    /// <summary>
-    /// 序列号
-    /// </summary>
-    public string SerialNumber { get; set; }
-    
-    /// <summary>
-    /// 型号
-    /// </summary>
-    public string Model { get; set; }
-    
-    /// <summary>
-    /// 制造商
-    /// </summary>
-    public string Manufacturer { get; set; }
+	/// <summary>
+	/// 序列号
+	/// </summary>
+	public string SerialNumber { get; set; }
 
+	/// <summary>
+	/// 型号
+	/// </summary>
+	public string Model { get; set; }
+
+	/// <summary>
+	/// 制造商
+	/// </summary>
+	public string Manufacturer { get; set; }
+
+	public string Guid { get; set; }
+
+	public string UUID { get; set; }
 }
