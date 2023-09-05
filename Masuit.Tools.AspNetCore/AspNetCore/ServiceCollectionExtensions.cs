31a8eb7f65a05d5bd0cd48de6dfcf36712de48a3
懒得勤快 <admin@masuit.com>
2023-09-05 15:48:46 +0800

    升级包

--- Masuit.Tools.AspNetCore/AspNetCore/ServiceCollectionExtensions.cs
+++ Masuit.Tools.AspNetCore/AspNetCore/ServiceCollectionExtensions.cs
@@ -12,101 +12,112 @@ namespace Masuit.Tools.Core.AspNetCore;
 /// </summary>
 public static class ServiceCollectionExtensions
 {
-    /// <summary>
-    /// 注入断点续传服务
-    /// </summary>
-    /// <param name="services"></param>
-    /// <returns></returns>
-    public static IServiceCollection AddResumeFileResult(this IServiceCollection services)
-    {
-        services.TryAddSingleton<IActionResultExecutor<ResumePhysicalFileResult>, ResumePhysicalFileResultExecutor>();
-        services.TryAddSingleton<IActionResultExecutor<ResumeVirtualFileResult>, ResumeVirtualFileResultExecutor>();
-        services.TryAddSingleton<IActionResultExecutor<ResumeFileStreamResult>, ResumeFileStreamResultExecutor>();
-        services.TryAddSingleton<IActionResultExecutor<ResumeFileContentResult>, ResumeFileContentResultExecutor>();
-        return services;
-    }
+	/// <summary>
+	/// 注入断点续传服务
+	/// </summary>
+	/// <param name="services"></param>
+	/// <returns></returns>
+	public static IServiceCollection AddResumeFileResult(this IServiceCollection services)
+	{
+		services.TryAddSingleton<IActionResultExecutor<ResumePhysicalFileResult>, ResumePhysicalFileResultExecutor>();
+		services.TryAddSingleton<IActionResultExecutor<ResumeVirtualFileResult>, ResumeVirtualFileResultExecutor>();
+		services.TryAddSingleton<IActionResultExecutor<ResumeFileStreamResult>, ResumeFileStreamResultExecutor>();
+		services.TryAddSingleton<IActionResultExecutor<ResumeFileContentResult>, ResumeFileContentResultExecutor>();
+		return services;
+	}
 
-    /// <summary>
-    /// 注入7z压缩
-    /// </summary>
-    /// <param name="services"></param>
-    /// <returns></returns>
-    public static IServiceCollection AddSevenZipCompressor(this IServiceCollection services)
-    {
-        services.AddHttpClient<ISevenZipCompressor, SevenZipCompressor>();
-        return services;
-    }
+	/// <summary>
+	/// 注入7z压缩
+	/// </summary>
+	/// <param name="services"></param>
+	/// <returns></returns>
+	public static IServiceCollection AddSevenZipCompressor(this IServiceCollection services)
+	{
+		services.AddHttpClient<ISevenZipCompressor, SevenZipCompressor>();
+		return services;
+	}
 
-    /// <summary>
-    /// 自动扫描需要注册的服务，被ServiceInject标记的class可自动注入
-    /// </summary>
-    /// <param name="services"></param>
-    public static void AutoRegisterServices(this IServiceCollection services)
-    {
-        var assemblies = AppDomain.CurrentDomain.GetAssemblies();
-        services.RegisterServiceByAttribute(assemblies);
-        services.RegisterBackgroundService(assemblies);
-    }
+	/// <summary>
+	/// 自动扫描需要注册的服务，被ServiceInject标记的class可自动注入
+	/// </summary>
+	/// <param name="services"></param>
+	public static void AutoRegisterServices(this IServiceCollection services)
+	{
+		var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+		services.RegisterServiceByAttribute(assemblies);
+		services.RegisterBackgroundService(assemblies);
+	}
 
-    /// <summary>
-    /// 通过 ServiceAttribute 批量注册服务
-    /// </summary>
-    /// <param name="services"></param>
-    private static void RegisterServiceByAttribute(this IServiceCollection services, IEnumerable<Assembly> assemblies)
-    {
-        var types = assemblies.SelectMany(t => t.GetTypes()).Where(t => t.GetCustomAttributes(typeof(ServiceInjectAttribute), false).Length > 0 && t.IsClass && !t.IsAbstract).ToList();
+	/// <summary>
+	/// 自动扫描需要注册的服务，被ServiceInject标记的class可自动注入
+	/// </summary>
+	/// <param name="services"></param>
+	/// <param name="assemblies"></param>
+	public static void AutoRegisterServices(this IServiceCollection services, params Assembly[] assemblies)
+	{
+		services.RegisterServiceByAttribute(assemblies);
+		services.RegisterBackgroundService(assemblies);
+	}
 
-        foreach (var type in types)
-        {
-            var typeInterface = type.GetInterfaces().FirstOrDefault();
-            if (typeInterface == null)
-            {
-                //服务非继承自接口的直接注入
-                switch (type.GetCustomAttribute<ServiceInjectAttribute>().Lifetime)
-                {
-                    case ServiceLifetime.Singleton: services.AddSingleton(type); break;
-                    case ServiceLifetime.Scoped: services.AddScoped(type); break;
-                    case ServiceLifetime.Transient: services.AddTransient(type); break;
-                }
-            }
-            else
-            {
-                //服务继承自接口的和接口一起注入
-                switch (type.GetCustomAttribute<ServiceInjectAttribute>().Lifetime)
-                {
-                    case ServiceLifetime.Singleton: services.AddSingleton(typeInterface, type); break;
-                    case ServiceLifetime.Scoped: services.AddScoped(typeInterface, type); break;
-                    case ServiceLifetime.Transient: services.AddTransient(typeInterface, type); break;
-                }
-            }
-        }
-    }
+	/// <summary>
+	/// 通过 ServiceAttribute 批量注册服务
+	/// </summary>
+	/// <param name="services"></param>
+	private static void RegisterServiceByAttribute(this IServiceCollection services, IEnumerable<Assembly> assemblies)
+	{
+		var types = assemblies.SelectMany(t => t.GetTypes()).Where(t => t.GetCustomAttributes(typeof(ServiceInjectAttribute), false).Length > 0 && t.IsClass && !t.IsAbstract).ToList();
 
-    /// <summary>
-    /// 注册后台服务
-    /// </summary>
-    /// <param name="services"></param>
-    private static void RegisterBackgroundService(this IServiceCollection services, IEnumerable<Assembly> assemblies)
-    {
-        var types = assemblies.SelectMany(t => t.GetTypes()).Where(t => typeof(BackgroundService).IsAssignableFrom(t) && !t.IsAbstract);
-        foreach (var type in types)
-        {
-            services.AddSingleton(typeof(IHostedService), type);
-        }
-    }
+		foreach (var type in types)
+		{
+			var typeInterface = type.GetInterfaces().FirstOrDefault();
+			if (typeInterface == null)
+			{
+				//服务非继承自接口的直接注入
+				switch (type.GetCustomAttribute<ServiceInjectAttribute>().Lifetime)
+				{
+					case ServiceLifetime.Singleton: services.AddSingleton(type); break;
+					case ServiceLifetime.Scoped: services.AddScoped(type); break;
+					case ServiceLifetime.Transient: services.AddTransient(type); break;
+				}
+			}
+			else
+			{
+				//服务继承自接口的和接口一起注入
+				switch (type.GetCustomAttribute<ServiceInjectAttribute>().Lifetime)
+				{
+					case ServiceLifetime.Singleton: services.AddSingleton(typeInterface, type); break;
+					case ServiceLifetime.Scoped: services.AddScoped(typeInterface, type); break;
+					case ServiceLifetime.Transient: services.AddTransient(typeInterface, type); break;
+				}
+			}
+		}
+	}
+
+	/// <summary>
+	/// 注册后台服务
+	/// </summary>
+	/// <param name="services"></param>
+	private static void RegisterBackgroundService(this IServiceCollection services, IEnumerable<Assembly> assemblies)
+	{
+		var types = assemblies.SelectMany(t => t.GetTypes()).Where(t => typeof(BackgroundService).IsAssignableFrom(t) && !t.IsAbstract);
+		foreach (var type in types)
+		{
+			services.AddSingleton(typeof(IHostedService), type);
+		}
+	}
 }
 
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface)]
 public class ServiceInjectAttribute : Attribute
 {
-    public ServiceInjectAttribute()
-    {
-    }
+	public ServiceInjectAttribute()
+	{
+	}
 
-    public ServiceInjectAttribute(ServiceLifetime lifetime)
-    {
-        Lifetime = lifetime;
-    }
+	public ServiceInjectAttribute(ServiceLifetime lifetime)
+	{
+		Lifetime = lifetime;
+	}
 
-    public ServiceLifetime Lifetime { get; set; } = ServiceLifetime.Transient;
+	public ServiceLifetime Lifetime { get; set; } = ServiceLifetime.Transient;
 }
