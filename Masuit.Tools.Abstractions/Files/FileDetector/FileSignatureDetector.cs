c675dae08e4db7a81f39f2beafd9dac3e9a56801
懒得勤快 <admin@masuit.com>
2024-08-09 11:26:15 +0800

    修正文件真实类型检测的bug

--- Masuit.Tools.Abstractions/Files/FileDetector/FileSignatureDetector.cs
+++ Masuit.Tools.Abstractions/Files/FileDetector/FileSignatureDetector.cs
@@ -3,86 +3,87 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using Masuit.Tools.Reflection;
 using Masuit.Tools.Systems;
 
 namespace Masuit.Tools.Files.FileDetector;
 
 public static class FileSignatureDetector
 {
-    private static List<IDetector> Detectors { get; set; } = [];
+	private static List<IDetector> Detectors { get; set; } = [];
 
-    public static IReadOnlyList<IDetector> Registered => Detectors;
+	public static IReadOnlyList<IDetector> Registered => Detectors;
 
-    public static void AddDetector<T>() where T : IDetector
-    {
-        var instance = Activator.CreateInstance<T>();
-        AddDetector(instance);
-    }
+	public static void AddDetector<T>() where T : IDetector
+	{
+		var instance = Activator.CreateInstance<T>();
+		AddDetector(instance);
+	}
 
-    public static void AddDetector(IDetector instance)
-    {
-        if (!Detectors.Contains(instance))
-        {
-            Detectors.Add(instance);
-        }
-    }
+	public static void AddDetector(IDetector instance)
+	{
+		if (!Detectors.Contains(instance))
+		{
+			Detectors.Add(instance);
+		}
+	}
 
-    static FileSignatureDetector()
-    {
-        var detectorTypeInfo = typeof(IDetector).GetTypeInfo();
-        foreach (var type in AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.DefinedTypes))
-        {
-            if (detectorTypeInfo.IsAssignableFrom(type) && !type.IsAbstract && type.DeclaredConstructors.First().GetParameters().Length == 0)
-            {
-                AddDetector(Activator.CreateInstance(type.AsType()) as IDetector);
-            }
-        }
-    }
+	static FileSignatureDetector()
+	{
+		var type = typeof(IDetector);
+		foreach (var item in AppDomain.CurrentDomain.GetAssemblies().Where(a => !a.FullName.StartsWith(["System", "Microsoft"])).SelectMany(a => a.GetLoadableExportedTypes()))
+		{
+			if (type.IsAssignableFrom(item) && !item.IsAbstract && !item.IsInterface && item.GetTypeInfo().DeclaredConstructors.First().GetParameters().Length == 0)
+			{
+				AddDetector(Activator.CreateInstance(item) as IDetector);
+			}
+		}
+	}
 
-    public static IDetector DetectFiletype(string filepath)
-    {
-        using var stream = File.OpenRead(filepath);
-        return DetectFiletype(stream);
-    }
+	public static IDetector DetectFiletype(string filepath)
+	{
+		using var stream = File.OpenRead(filepath);
+		return DetectFiletype(stream);
+	}
 
-    public static IDetector DetectFiletype(this FileInfo file)
-    {
-        using var stream = file.OpenRead();
-        return DetectFiletype(stream);
-    }
+	public static IDetector DetectFiletype(this FileInfo file)
+	{
+		using var stream = file.OpenRead();
+		return DetectFiletype(stream);
+	}
 
-    public static IDetector DetectFiletype(this Stream stream)
-    {
-        if (stream.CanSeek)
-        {
-            string pre = null;
-            IDetector foundDetector = new NoneDetector();
-            while (true)
-            {
-                bool found = false;
-                foreach (var detector in Detectors.Where(d => d.Precondition == pre))
-                {
-                    stream.Position = 0;
-                    if (detector.Detect(stream))
-                    {
-                        found = true;
-                        foundDetector = detector;
-                        pre = detector.Extension;
-                        break;
-                    }
-                }
-                if (!found)
-                {
-                    break;
-                }
-            }
+	public static IDetector DetectFiletype(this Stream stream)
+	{
+		if (stream.CanSeek)
+		{
+			string pre = null;
+			IDetector foundDetector = new NoneDetector();
+			while (true)
+			{
+				bool found = false;
+				foreach (var detector in Detectors.Where(d => d.Precondition == pre))
+				{
+					stream.Position = 0;
+					if (detector.Detect(stream))
+					{
+						found = true;
+						foundDetector = detector;
+						pre = detector.Extension;
+						break;
+					}
+				}
+				if (!found)
+				{
+					break;
+				}
+			}
 
-            stream.Position = 0;
-            return foundDetector;
-        }
+			stream.Position = 0;
+			return foundDetector;
+		}
 
-        using var ms = new PooledMemoryStream();
-        stream.CopyTo(ms);
-        return DetectFiletype(ms);
-    }
+		using var ms = new PooledMemoryStream();
+		stream.CopyTo(ms);
+		return DetectFiletype(ms);
+	}
 }
---
+++ Masuit.Tools.Net45/Config/CoreConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+
+namespace Masuit.Tools.Config
+{
+    public static class ConfigHelper
+    {
+        public static string GetConfigOrDefault(string key, string defaultValue = "")
+        {
+            return ConfigurationManager.AppSettings.Get(key) ?? defaultValue;
+        }
+    }
+}
