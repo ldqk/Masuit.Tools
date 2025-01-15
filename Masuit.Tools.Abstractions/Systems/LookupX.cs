c1743d821567f5420285c6534190ad6b79c4cbb5
懒得勤快 <admin@masuit.com>
2025-01-15 11:47:37 +0800

    优化LookupX

--- Masuit.Tools.Abstractions/Systems/LookupX.cs
+++ Masuit.Tools.Abstractions/Systems/LookupX.cs
@@ -5,7 +5,7 @@ using System.Linq;
 
 namespace Masuit.Tools.Systems;
 
-public class LookupX<TKey, TElement> : IEnumerable<KeyValuePair<TKey, List<TElement>>>
+public class LookupX<TKey, TElement> : IEnumerable<Grouping<TKey, TElement>>
 {
     private readonly IDictionary<TKey, List<TElement>> _dictionary;
 
@@ -19,9 +19,9 @@ public class LookupX<TKey, TElement> : IEnumerable<KeyValuePair<TKey, List<TElem
         _dictionary = dic;
     }
 
-    public IEnumerator<KeyValuePair<TKey, List<TElement>>> GetEnumerator()
+    public IEnumerator<Grouping<TKey, TElement>> GetEnumerator()
     {
-        return _dictionary.Select(pair => pair).GetEnumerator();
+        return _dictionary.Select(pair => new Grouping<TKey, TElement>(pair.Key, pair.Value)).GetEnumerator();
     }
 
     IEnumerator IEnumerable.GetEnumerator()
@@ -37,4 +37,33 @@ public class LookupX<TKey, TElement> : IEnumerable<KeyValuePair<TKey, List<TElem
     public int Count => _dictionary.Count;
 
     public List<TElement> this[TKey key] => _dictionary.TryGetValue(key, out var value) ? value : new List<TElement>();
+}
+
+public class Grouping<TKey, TElement> : IEnumerable<TElement>
+{
+    private readonly List<TElement> _list;
+
+    internal Grouping(TKey key, List<TElement> list)
+    {
+        Key = key;
+        _list = list;
+    }
+
+    public IEnumerator<TElement> GetEnumerator()
+    {
+        return _list.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public TKey Key { get; }
+
+    public void Deconstruct(out TKey key, out IEnumerable<TElement> elements)
+    {
+        key = Key;
+        elements = _list;
+    }
 }
